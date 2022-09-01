e22c4d6882383e932458ac62ad7c2313f2e2c088
Semirekov Evgenii <ekar@yandex.ru>
2022-09-01 08:09:31 +0300

    Реализовал модуль в виде класса.

--- hh.py
+++ hh.py
@@ -1,57 +1,50 @@
-from math import ceil
-
 from utils_request import get_json_from_api_request
 
+class VacanceProvider:
 
-def read_vacancies_from_hh(language, area=1, date='', page=0, per_page=1):
-    base_url = 'https://api.hh.ru/vacancies/'
+    def __init__(self) -> None:
+        self.__base_url = 'https://api.hh.ru/vacancies/'
+        self.vacancies_count = 0
 
-    params = {
-        'area': area,
-        'specialization': 1,
-        'page': page,
-        'per_page': per_page,
-        'text': language,
-        'search_field': 'name',
-        'date_from': date
-    }
+
+    def _read_vacancies_from_api(self, key_world, area=1, date='', page=0, per_page=1):
+        params = {
+            'area': area,
+            'specialization': 1,
+            'page': page,
+            'per_page': per_page,
+            'text': key_world,
+            'search_field': 'name',
+            'date_from': date
+        }
     
-    return get_json_from_api_request(base_url, None, params)
+        return get_json_from_api_request(self.__base_url, None, params)
 
-
-def salary_exist(vacance):
-    salary = vacance['salary']
-    return salary and salary['currency'] == 'RUR'        
-
-
-def get_vacance_count(language, area, date=''):
-    return read_vacancies_from_hh(language, area, date, 0, 1)['found']     
-
-
-def get_pages_count(found, respone_limit, per_page):        
-    page_count = ceil(found / per_page)        
-    page_count_max = ceil(respone_limit / per_page)
-
-    return min(page_count, page_count_max)
-       
-
-def get_all_vacancies(language, area, date):
-    respone_limit, per_page = 2000, 100    
-    vacance_count = get_vacance_count(language, area, date)    
-    page_count = get_pages_count(vacance_count, respone_limit, per_page)
-
-    vacancies = []
-    for page_index in range(page_count):
-        vacancies += read_vacancies_from_hh(language, area, date, page_index, per_page)['items']
     
-    return vacancies
+    def _salary_exist(self, vacance):
+        salary = vacance['salary']
+        return salary and salary['currency'] == 'RUR'  
 
 
-def get_salary(vacance):
-    return [vacance['salary']['from'], vacance['salary']['to']]
+    def _get_single_salary(self, vacance):
+        return [vacance['salary']['from'], vacance['salary']['to']]
+
+    
+    def get_all_vacancies(self, language, area, date):
+        page_index, per_page  = 0, 100        
+        raw_vacancies = self._read_vacancies_from_api(language, area, date, page_index, per_page)
+        
+        self.vacancies_count  = raw_vacancies['found']
+        page_count = raw_vacancies['pages']
+        vacancies = raw_vacancies['items']    
+    
+        for page_index in range(1, page_count):
+            vacancies += self._read_vacancies_from_api(language, area, date, page_index, per_page)['items']            
+    
+        return vacancies
 
 
-def get_all_salary(language, area, date=''):        
-    vaсancies = get_all_vacancies(language, area, date)
-    return [get_salary(vacance) for vacance in vaсancies if salary_exist(vacance)]
+    def get_all_salary(self, language, area, date=''):                
+        vaсancies = self.get_all_vacancies(language, area, date)        
+        return [self._get_single_salary(vacance) for vacance in vaсancies if self._salary_exist(vacance)]
 
